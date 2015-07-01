9570441f6dfbccaa579336dccfbad94c6ad04208
Andreas Plieninger <plieningerweb@gmail.com>
2015-07-01 23:14:37 +0200

    move python script to right location

---
+++ python-scripts/getlivedata.py
@@ -0,0 +1,12 @@
+#get live data from jEAR interface
+
+#idea is to get livedata from camera and calculate stuff from there
+#problem is: maybe possible but quite hard
+#better would be to rewrite cuda (c project): the main loop
+#it is exactly created for that purpose
+
+#we still can use python to record something and try to run it then
+#once it is working, we will do live stuff
+
+#we even can use a remote control to automagically start and stop logging
+#check https://github.com/tpietzsch/jAER/blob/master/host/python/logdata.py
